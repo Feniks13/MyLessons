b26e9b674c82e2d3714f56c07db7f83ee1db6d49
Sergey Privalov <kalabanga12@yandex.ru>
2019-08-22 16:19:17 +0500

    Правки по основному

--- lesson_03/js/main.js
+++ lesson_03/js/main.js
@@ -6,16 +6,16 @@ let money = +prompt('Ваш месячный доход?'),  /* Спрашива
     cost = +prompt('Во сколько это обойдется?'),
     obligatory2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
     cost2 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = money - cost,   /* Вычисляем бюджет на месяц */
+    budgetMonth = money - cost - cost2,   /* Вычисляем бюджет на месяц */
     mission = 10000,
     month = Math.ceil(mission / budgetMonth),  /* Округляем в большую стороную Вычисляем количество месяцев */
     budgetDay = Math.floor(budgetMonth / 30); /* Округляем в меньшую сторону. Вычисляем бюджет на день */
   
 if (budgetDay > 800) {
   console.log('Высокий уровень дохода');  
-} else if (800 > budgetDay && budgetDay > 300) {
+} else if (800 >= budgetDay && budgetDay >= 300) {
   console.log('Средний уровень дохода');
-} else if (300 > budgetDay && budgetDay > 0) {
+} else if (300 > budgetDay && budgetDay >= 0) {
   console.log('Низкий уровень дохода');
 } else {
   console.log('Что то пошло не так');
@@ -28,7 +28,6 @@ console.log(obligatory);
 console.log(cost);
 console.log(obligatory2);
 console.log(cost2);
-console.log(budgetMonth);
 console.log('Цель будет достигнута за ' + month + ' месяцев');
 console.log('Ежедневный расход составит ' + budgetDay + ' денег');
 
